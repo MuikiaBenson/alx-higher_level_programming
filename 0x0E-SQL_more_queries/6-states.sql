55142070e282c1c303b2369b110a8f43ce74a233
Muikia Benson <benmuigai20@gmail.com>
2024-03-16 09:40:43 +0300

    6-state

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- Creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
---Use the database
+-- Use the database
 USE hbtn_0d_usa;
+-- Create table
 CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));

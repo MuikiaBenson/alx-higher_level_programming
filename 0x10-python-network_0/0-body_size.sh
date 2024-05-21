04cee05e3e756f36ff02f0051cef66ec7d065dfe
Muikia Benson <benmuigai20@gmail.com>
2024-05-21 19:11:01 +0300

    sends a req to URL and displays the size of the response body

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script takes in a URL, sends a request to that URL using curl,
+# and displays the size of the body of the response in bytes.
+curl -so /dev/null -w '%{size_download}\n' $1

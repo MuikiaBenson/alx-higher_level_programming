77d20fb8854bca51ecced8dcde5a4faa5142a355
Muikia Benson <benmuigai20@gmail.com>
2024-05-23 09:20:02 +0300

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,20 +1,27 @@
+#!/usr/bin/python3
+"""Finds the peak in an unsorted list of integers"""
+
+#!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers."""
+
+
 def find_peak(list_of_integers):
-    """
-    Finds a peak in a list of unsorted integers.
-    """
+    """Finds a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
+    return _find_peak(list_of_integers, 0, len(list_of_integers) - 1)
 
-    mid = len(list_of_integers) // 2
 
-    # Check if mid element is peak
-    if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
-       (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
-        return list_of_integers[mid]
+def _find_peak(lint, start, stop):
+    """Recursively finds a peak in the list."""
+    if start == stop:
+        return lint[start]
 
-    # If right neighbor is greater, search right half
-    if mid < len(list_of_integers) - 1 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid + 1:])
-    
-    # Otherwise, search left half
-    return find_peak(list_of_integers[:mid])
+    mid = (start + stop) // 2
+
+    if mid > 0 and lint[mid] < lint[mid - 1]:
+        return _find_peak(lint, start, mid - 1)
+    elif mid < len(lint) - 1 and lint[mid] < lint[mid + 1]:
+        return _find_peak(lint, mid + 1, stop)
+    else:
+        return lint[mid]
